c4fa2c0ea73d1f787e5645b4d48c78224241a18c
Ng-Vanh <nguyenvietanh2234@gmail.com>
2023-02-19 21:54:19 +0700

    Game First commit

---
+++ GamePTPM/MainObject.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "CommonFunc.h";
+#include "BaseObject.h";
+
+#define GRAVITY_SPEED 0.4
+#define MAX_FAIL_SPEED 10
+#define PLAYER_SPEED 6
+#define PLAYER_JUMP_VALUE 15
+
+class MainObject :public BaseObject
+{
+public:
+	MainObject();
+	~MainObject();
+
+	enum WalkType
+	{
+		WALK_NONE = 0,
+		WALK_RIGHT = 1,
+		WALK_LEFT = 2,
+	};
+	bool LoadImg(std::string path, SDL_Renderer* screen);
+	void Show(SDL_Renderer* des);
+	void HandleInputAction(SDL_Event events, SDL_Renderer* screen);
+	void set_clip();
+
+	void DoPlayer(Map& map_data);
+	void CheckToMap(Map& map_data);// kiem tra map 
+	void SetMapXY(const int map_x, const int map_y) // luu vi tri ban do
+	{
+		map_x_ = map_x;
+		map_y_ = map_y;
+	}
+	void CenterEnityOnMap(Map& map_data);
+	void UpDateImagePlayer(SDL_Renderer* des);
+
+private:
+	float x_val_;// khi bam di chuyen 1 doan bang x_val
+	float y_val_;
+
+	float x_pos_;
+	float y_pos_;
+
+	int width_frame_;
+	int height_frame_;
+
+	SDL_Rect frame_clip_[8];// luu cac frame. 8mla 1 anh co 8 chuyen dong
+	Input input_type_;
+	int frame_;
+	int status_;// trang thai kieu di chuyen hien tai
+	bool on_ground_;
+
+	int map_x_;
+	int map_y_;
+
+
+	int come_back_time = 0;
+};
---
+++ GamePTPM/main.cpp
@@ -0,0 +1,121 @@
+#include"SDL.h"
+#include"BaseObject.h"
+#include "CommonFunc.h"
+#include"GameMap.h"
+#include"MainObject.h"
+#include"ImpTimer.h"
+BaseObject g_background;
+
+void close()
+{
+	g_background.Free();
+	SDL_DestroyRenderer(g_screen);
+	g_screen = NULL;
+	SDL_DestroyWindow(g_Window);
+	g_Window = NULL;
+	IMG_Quit();
+	SDL_Quit();
+}
+
+bool InitData()
+{
+	bool success = true;
+	int ret = SDL_Init(SDL_INIT_VIDEO);
+	if (ret < 0) return false;
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+	g_Window = SDL_CreateWindow("Gamecpp", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (g_Window == NULL)
+	{
+		success = false;
+	}
+	else
+	{
+		g_screen = SDL_CreateRenderer(g_Window, -1, SDL_RENDERER_ACCELERATED);
+		if (g_screen == NULL) success = false;
+		else
+		{
+			SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+			int imgFlags = IMG_INIT_PNG;
+			if (!(IMG_Init(imgFlags) && imgFlags))
+				success = false;
+		}
+	}
+	return success;
+}
+bool LoadBackGround()
+{
+	bool ret = g_background.LoadImg("img/bk1.png", g_screen);
+	if (ret == false)
+		return false;
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	ImpTimer fps_timer;
+
+
+
+	if (InitData() == false)
+		return -1;
+	if (LoadBackGround() == false)
+		return -1;
+
+	GameMap game_map;
+	char nameFileMap[] = "map/map01.dat";
+	game_map.LoadMap(nameFileMap);
+	game_map.LoadTiles(g_screen);
+
+	MainObject p_player;
+	p_player.LoadImg("img//run_right.png", g_screen);
+	p_player.set_clip();
+
+
+
+	bool is_quit = false;
+	while (!is_quit)
+	{
+		fps_timer.start();
+		while (SDL_PollEvent(&g_event) != 0)
+		{
+			if (g_event.type == SDL_QUIT)
+			{
+				is_quit = true;
+			}
+
+
+			p_player.HandleInputAction(g_event, g_screen);
+		}
+		SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+		SDL_RenderClear(g_screen);
+
+		g_background.Render(g_screen, NULL);
+
+		Map map_data = game_map.getMap();
+
+		p_player.SetMapXY(map_data.start_x_, map_data.start_y_);
+		p_player.DoPlayer(map_data);
+		p_player.Show(g_screen);
+
+		game_map.setMap(map_data);
+		game_map.DrawMap(g_screen);
+
+		SDL_RenderPresent(g_screen);
+
+		int real_ipm_time = fps_timer.get_ticks();
+		int time_one_frame = 1000 / FPS;// ms
+		if (real_ipm_time < time_one_frame)
+		{
+			int delay_time = time_one_frame - real_ipm_time;
+			if (delay_time >= 0)
+			{
+				SDL_Delay(delay_time);
+			}
+			std::cout << FPS << std::endl;
+		}
+	}
+	close();
+	return 0;
+}
